da6928a18134108e35c376bb1f86850f2b09158b
Yasmin <liz2015lop@gmail.com>
2022-06-04 15:49:54 -0600

    setting up user documents

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
     GoogleAuthProvider 
 } from 'firebase/auth';
 
+import {
+    getFirestore,
+    doc, //get data
+    getDoc, //access data
+    setDoc
+} from 'firebase/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAAS6bKAPoyDL5fUkfFOgpsnzwm_nL5ij0",
     authDomain: "crwn-clothing-db-1a446.firebaseapp.com",
@@ -25,3 +32,32 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch(error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+}
